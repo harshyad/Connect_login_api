267b107a0a5b030844aec47ff8baeb5cde597339
Harsh Yadav <harshyadav78200@gmail.com>
2023-04-13 16:38:48 +0530

    removed send mail

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from api import views
 
+
 urlpatterns = [
     # path('facelogin/',views.facelogin,name='facelogin'),
-    path('login/',views.login,name='login'),
-    path('register/',views.Register,name='regsiter'),
-    path('update/',views.update,name='update'),
-    path('view/',views.view,name='view')
+    path('login',views.login,name='login'),
+    path('register',views.Register,name='regsiter'),
+    path('update',views.update,name='update'),
+    path('view',views.view,name='view')
 ]
+
 # dlib==19.24.1
 # face-recognition==1.3.0
 # face-recognition-models==0.3.0

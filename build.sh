4a94ac7bc9a2e2af5abad87e647c661f4d211ea0
Harsh Yadav <harshyadav78200@gmail.com>
2023-04-14 23:13:48 +0530

    changes in deps.txt

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 # Install Dependencies
-pip install WHEEL
-
 pip install -r requirements.txt
 
+apt install build-essential cmake libopenblas-dev liblapack-dev libx11-dev libgtk-3-dev
+
 # Install libraries
-# pip install dlib==19.24.1
-# sudo apt install build-essential cmake libopenblas-dev liblapack-dev libx11-dev libgtk-3-dev
+pip install dlib==19.24.1
+pip install face-recognition==1.3.0
+pip install face-recognition-models==0.3.0

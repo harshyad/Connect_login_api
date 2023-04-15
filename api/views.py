80038d740b92dd35f4d2a2c4be508710785808aa
Harsh Yadav <harshyadav78200@gmail.com>
2023-04-15 10:32:39 +0530

    Done

--- api/views.py
+++ api/views.py
@@ -423,7 +423,7 @@ def Register(request):
                             roll_no = serializer.data['roll_no']
                             password = serializer.data['password']
                             user_type = serializer.data['user_type']
-                            sendMail(user_type, name, roll_no, password, email)
+                            # sendMail(user_type, name, roll_no, password, email)
                             return Response({"status": True, "data": serializer.data})
                         return Response({"status":False,"error":serializer.errors})
 
@@ -435,7 +435,7 @@ def Register(request):
                             email = serializer.data['email']
                             password = serializer.data['password']
                             user_type = serializer.data['user_type']
-                            sendMail(user_type, name, email, password)
+                            # sendMail(user_type, name, email, password)
                             return Response({"status": True,"data": serializer.data})
                         return Response({"status":False,"error":serializer.errors})
                     else:

e3f72a0f3b403baaf607f85edc9532fdcdf7983a
Harsh Yadav <harshyadav78200@gmail.com>
2023-04-15 22:40:10 +0530

    Done

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # Install Dependencies
 pip install -r requirements.txt
 
-pip install -e -e git+https://github.com/davisking/dlib.git@5f7026ab52a1761a7d181af90a59b1968a0c2dcd#egg=dlib
+pip install -e git+https://github.com/davisking/dlib.git@5f7026ab52a1761a7d181af90a59b1968a0c2dcd#egg=dlib
 
 pip install face-recognition==1.3.0
 

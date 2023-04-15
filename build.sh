e95be2c551d7aa796efa80b3d38886a05f4fa413
Harsh Yadav <harshyadav78200@gmail.com>
2023-04-15 18:53:36 +0530

    Done

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 # Install Dependencies
 pip install -r requirements.txt
 
-apt-get install build-essential cmake libopenblas-dev liblapack-dev libx11-dev libgtk-3-dev
-
-# Install libraries
-pip install dlib==19.24.1
-pip install face-recognition==1.3.0
-pip install face-recognition-models==0.3.0

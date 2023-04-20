3f460417f0cd75944ff7b2e324afd5ff458d1223
Harsh Yadav <harshyadav78200@gmail.com>
2023-04-20 17:36:53 +0530

    Dockerize the django application

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 # Install Dependencies
-docker-compose up -d
+# docker-compose up -d
+docker info
+
+# pip install -r requirements.txt
+
+# sudo apt install libavdevice-dev libavfilter-dev libavformat-dev
+
+# pip install -e git+https://github.com/davisking/dlib.git@5f7026ab52a1761a7d181af90a59b1968a0c2dcd#egg=dlib
+
+
+# pip install face-recognition==1.3.0
+
+# face-recognition-models==0.3.0
